78accbe428f3bec117ba5cabd7ce4af025b27e05
RaykujanK13 <lancarjaya1928@gmail.com>
2023-04-06 17:26:18 +0700

    SaWebsite Update V2.4.4 RestZa :white_check_mark:
    Update SaWebsite Add Feature & Fixed Bugs

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # RaykujanK13
 Rebuild Website for Sayuchan the Discord Bot.
 
-# PLEASE READ THIS NOTE
-Note: for the next update the source code will not be published and for the vanilla version we will remain open source
+# Installation =======================
+Note Install NodeJS (Minimum NodeJS 12)
+## Step 1 Clonening
+clone the project to your pc/computer
+## Step 2 Input Your Token Discord.js
+input your token discord bot to config.js
+## Step 3 Install All Package
+Type npm install in terminal/command prompt 
+(NOTE : You must install NodeJS if you want to run the npm prefix)
+## Step 4 Start Project
+Type npm start in terminal/command prompt
+(NOTE : You must install NodeJS if you want to run the npm prefix)
+## Step 5 Final Installation
+Copy link host
+Example : http://localhot:3000
+# ====================================
 
 #Vanilla Version
 https://github.com/rimba5446/sayuchan-website
 
 ## View the Sayuchan website here!
-http://sayuchan.glitch.me (SaWebsite Update V2.4.4 RestZa)
+http://sayuchan.glitch.me
 
 # Github
 htpps://github.com/rimba5446
--- index.js
+++ index.js
@@ -1,31 +1,67 @@
 const discord = require("discord.js");
+const moment = require('moment');
+require('moment-duration-format');
+const { ChannelType, version} = require('discord.js');
 const client = new discord.Client();
-const { token } = require("./config.json");
+const { token, client_port, prefix, url_server, url_invite } = require("./config.json");
+const port = client_port || 3001;
 // const { render } = require('ejs');
 const express = require('express');
 const app = express();
 // const path = require('path');
 const router = express.Router();
-app.use(express.static("styles"));
-app.use(express.static("assets"));
+app.use(express.static("styles/"));
 app.set('view engine', 'ejs');
 
+//protected file
+function userIsAllowed(callback) {
+    callback(false);
+  };
+  
+  var protectPath = function(regex) {
+    return function(req, res, next) {
+      if (!regex.test(req.url)) { return next(); }
+  
+      userIsAllowed(function(allowed) {
+        if (allowed) {
+          next();
+        } else {
+          res.end('What are you doing');
+        }
+      });
+    };
+  };
+  app.use(protectPath(/^\/assets\/.*$/));
 
+//Change log Update
+const contentUpdate1 ="SaWebsite Update V1.5.4";
+const contentUpdate2 ="Fixed Bug Interaction & Api Callback Break";
+const contentUpdate3 ="~";
+const titleUpdate ="Sayuchan Update V1.4.5";
+
+//AboutSayuchan
+// const aboutSayuchan ="testing Content";
+//AboutContent
+const titleAbout ="RaykujanK13";
+const contentAbout = "Hello, I'm RaykujanK13! Creator of Sayuchan and this website! Thank you for using Sayuchan and I hope it lives up to your expectations!";
+
+//Status Project
 const status = {
     status: 'Active',
 }
 
-const prefix = {
-    prefix: 's!',
-}
+app.use(async function(req, res, next) {
+    req.client = client;
+    next();
+});
 
-// index page
+// pages
 app.get('/', function(req, res) {
     res.render('index', {client: client});
 });
 
 app.get('/docs', function(req, res) {
-    res.render('docs', {client: client, prefix: prefix});
+    res.render('docs', {client: client, prefix: prefix, titleAbout:titleAbout, contentAbout:contentAbout, contentUpdate1:contentUpdate1, contentUpdate2:contentUpdate2, contentUpdate3:contentUpdate3, titleUpdate:titleUpdate});
 });
 
 app.get('/commands', function(req, res) {
@@ -33,32 +69,31 @@ app.get('/commands', function(req, res) {
 });
 
 app.get('/stats', function(req, res) {
-    res.render('stats', {client: client, status: status});
+    res.render('stats', {client: client, status: status, uptime: moment.duration(req.client.uptime).format(' D [days], H [hours], m [minutes], s [seconds]'), channelType: ChannelType, djsVersion: version});
 });
 
-// app.get('/invite', function(req, res) {
-//     res.render('invite');
-// });
+app.get('/invite', function(req, res) {
+    res.render('invite',{client: client, url_invite});
+});
 
-// app.get('/support', function(req, res) {
-//     res.render('support');
-// });
+app.get('/support', function(req, res) {
+    res.render('support',{client: client, url_server: url_server});
+});
 
 app.use(function (req, res) {
-    res.status(404).render('404');
+    res.status(404).render('404_old',{client: client});
 });
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
-
-client.on("ready", () => {
-    client.user.setStatus("online"); // You Can Set It To dnd, online, idle. dont set it to offline plz  
+app.listen(port, async function() {
+    console.log(`Running at Port: ${port}`);
+    console.log(`http://localhost:${port}`);
   });
 
-// client.on("ready", () => {
-//     console.log("I am Looking Forward to this :D")
-//   })
+client.on("ready", () => {
+    console.log("there's someone i like :D")
+  })
 
+  
 client.login(token)
-console.log('Running at Port 3000');
